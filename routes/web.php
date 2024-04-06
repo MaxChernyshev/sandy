106c1a74ac3e2f24d152c045e4cf7e4fed188f25
MaxChernyshev <maksim.chernyshev@gmail.com>
2024-04-06 11:55:39 -0700

    Front Main Controller added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [MainController::class, 'index'])->name('mainPage');
